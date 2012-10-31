55b1ecd1cec335248a9e685b79912ca48c213250
Henry Bradlow <hbradlow@berkeley.edu>
2012-10-31 15:21:32 -0700

    Fixed autograde admin

--- autograde_it/autograde_it/apps/autograde/admin.py
+++ autograde_it/autograde_it/apps/autograde/admin.py
@@ -3,7 +3,4 @@ from autograde.models import *
 admin.site.register(TestCase)
 admin.site.register(ProjectFile)
 admin.site.register(Project)
-admin.site.register(KVPair)
-admin.site.register(Result)
-admin.site.register(Submission)
 admin.site.register(TestResult)
