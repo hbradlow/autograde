c5b3e6a3c5c38acaabbdd31dd25d183b62c9d923
Henry Bradlow <hbradlow@berkeley.edu>
2012-11-06 10:28:34 -0800

    Added project files back to project create form

--- autograde_it/autograde_it/apps/autograde/forms.py
+++ autograde_it/autograde_it/apps/autograde/forms.py
@@ -14,9 +14,18 @@ class ProjectMetaForm(forms.ModelForm):
         model = ProjectMeta
         exclude = ("project",)
 class ProjectCreateForm(forms.ModelForm):
+    def __init__(self,*args,**kwargs):
+        if len(args)>=2:
+            self.project_files = args[1].getlist("project_files",[])
+        super(ProjectCreateForm,self).__init__(*args,**kwargs)
     def save(self,*args,**kwargs):
         super(ProjectCreateForm,self).save(*args,**kwargs)
         ProjectMeta.objects.create(project=self.instance)
+
+        #save the files
+        p = self.instance
+        for file in self.project_files:
+            ProjectFile.objects.create(project=p, file=file)
     class Meta:
         model = Project
         exclude = ("instructors",)
